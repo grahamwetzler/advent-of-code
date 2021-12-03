8046fcfa5bd08e82e1b2223144a8858f009e673d
Graham Wetzler <graham@wetzler.dev>
2021-12-02 21:54:28 -0600

    Add part 2 solution

--- 2021/02/solution.sql
+++ 2021/02/solution.sql
@@ -4,15 +4,16 @@ create table aoc.input_2021.input_02 (
 );
 
 with input as (
-    select *
+  select   row_number() over (order by 1) as row_num
+         , *
     from aoc.input_2021.input_02
 )
 
-, negated as (
+, part_1_step_1 as (
   select   case
            when direction in ('up', 'down') then 'depth'
            when direction in ('forward') then 'horizontal'
-            end as net_direction,
+            end as net_direction
          , case
            when direction = 'up' then units * -1
            else units
@@ -20,22 +21,38 @@ with input as (
     from input
 )
 
-, agg as (
+, part_1 as (
   select   net_direction
          , sum(units) as total
-    from negated
+    from part_1_step_1
    group by 1
 )
 
-, part_1 as (
-  select exp(sum(ln(total))) as answer -- why does SQL not have a product aggregate function!?
-    from agg
+, part_2 as (
+  select   direction
+         , case
+           when direction = 'down' then units
+           when direction = 'up' then units * -1
+            end as aim
+         , case
+           when direction = 'forward' then units
+            end as horizontal_position
+         , sum(aim) over (order by row_num) as current_aim
+         , sum(horizontal_position) over (order by row_num) as current_horizontal_position
+         , case
+           when direction = 'forward' then current_aim * horizontal_position
+            end as depth
+    from input
 )
 
 , answer as (
   select   'part 1' as part
-         , answer
+         , exp(sum(ln(total))) as answer /* why does SQL not have a product aggregate function!? */
     from part_1
+   union all
+  select   'part 2' as part
+         , sum(depth) * max(current_horizontal_position) as answer
+    from part_2
 )
 
 select *
