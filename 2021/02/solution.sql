5d650c1a8a49a5a743b26eb1f35894cd9e798ce0
Graham Wetzler <graham@wetzler.dev>
2021-12-02 21:13:59 -0600

    Fix indent

--- 2021/02/solution.sql
+++ 2021/02/solution.sql
@@ -9,33 +9,33 @@ with input as (
 )
 
 , negated as (
-    select   case
-             when direction in ('up', 'down') then 'depth'
-             when direction in ('forward') then 'horizontal'
-              end as net_direction,
-           , case
-             when direction = 'up' then units * -1
-             else units
-              end as units
-      from input
+  select   case
+           when direction in ('up', 'down') then 'depth'
+           when direction in ('forward') then 'horizontal'
+            end as net_direction,
+         , case
+           when direction = 'up' then units * -1
+           else units
+            end as units
+    from input
 )
 
 , agg as (
-    select   net_direction
-           , sum(units) as total
-      from negated
-     group by 1
+  select   net_direction
+         , sum(units) as total
+    from negated
+   group by 1
 )
 
 , part_1 as (
-    select exp(sum(ln(total))) as answer -- why does SQL not have a product aggregate function!?
-      from agg
+  select exp(sum(ln(total))) as answer -- why does SQL not have a product aggregate function!?
+    from agg
 )
 
 , answer as (
-select   'part 1' as part
-       , answer
-  from part_1
+  select   'part 1' as part
+         , answer
+    from part_1
 )
 
 select *
