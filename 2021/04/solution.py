78137cda09e3667d9366d547819bcbb8ac28ce33
Graham Wetzler <graham@wetzler.dev>
2021-12-04 16:53:17 -0600

    Add part 2 and cleanup

--- 2021/04/solution.py
+++ 2021/04/solution.py
@@ -1,35 +1,9 @@
-import re
-from pathlib import Path
 import pprint
 from dataclasses import dataclass
+from pathlib import Path
 
 BOARD_WIDTH = 5
 BOARD_HEIGHT = 5
-
-puzzle_input = Path("2021/04/input.txt").read_text()
-# puzzle_input = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-
-# 22 13 17 11  0
-#  8  2 23  4 24
-# 21  9 14 16  7
-#  6 10  3 18  5
-#  1 12 20 15 19
-
-#  3 15  0  2 22
-#  9 18 13 17  5
-# 19  8  7 25 23
-# 20 11 10 24  4
-# 14 21 16 12  6
-
-# 14 21 17 24  4
-# 10 16 15  9 19
-# 18  8 23 26 20
-# 22 11 13  6  5
-#  2  0 12  3  7"""
-
-numbers, *boards_str = puzzle_input.split("\n" * 2)
-
-drawn_numbers = [int(number) for number in numbers.split(",")]
 
 
 @dataclass
@@ -37,7 +11,7 @@ class Number:
     number: int
     marked: bool = False
 
-    def __init__(self, number):
+    def __init__(self, number: str):
         self.number = int(number)
 
     def __repr__(self) -> str:
@@ -48,10 +22,10 @@ class Number:
 class Board:
     numbers: list[list[Number]]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return pprint.pformat(self.numbers)
 
-    def update(self, drawn: int):
+    def update(self, drawn: int) -> None:
         for row in self.numbers:
             for number in row:
                 if number.number == drawn:
@@ -85,24 +59,32 @@ class Board:
                 for number in row:
                     if not number.marked:
                         score += number.number
+
         return score
 
 
-boards = []
+def parse_input() -> tuple:
+    puzzle_input = Path("2021/04/input.txt").read_text()
+    numbers, *boards_str = puzzle_input.split("\n" * 2)
+    drawn_numbers = [int(number) for number in numbers.split(",")]
 
-for board in boards_str:
-    board_list = []
-    for row in [i for i in board.split("\n") if i != ""]:
-        row_items = []
-        for column in [i for i in row.split(" ") if i != ""]:
-            number = Number(column)
-            row_items.append(number)
+    boards = []
 
-        board_list.append(row_items)
-    boards.append(Board(board_list))
+    for board in boards_str:
+        board_list = []
+        for row in [i for i in board.split("\n") if i != ""]:
+            row_items = []
+            for column in [i for i in row.split(" ") if i != ""]:
+                number = Number(column)
+                row_items.append(number)
+            board_list.append(row_items)
+        boards.append(Board(board_list))
+
+    return drawn_numbers, boards
 
 
-def part_1():
+def part_1() -> int:
+    drawn_numbers, boards = parse_input()
     for draw in drawn_numbers:
         for board in boards:
             board.update(draw)
@@ -110,4 +92,19 @@ def part_1():
                 return board.score * draw
 
 
-print(part_1())
+def part_2() -> int:
+    drawn_numbers, boards = parse_input()
+    board_scores = []
+    for draw in drawn_numbers:
+        for board in boards:
+            if not board.is_winner:
+                board.update(draw)
+                if board.is_winner:
+                    board_scores.append(board.score * draw)
+
+    return board_scores[-1]
+
+
+if __name__ == "__main__":
+    print("Part 1:", part_1())
+    print("Part 2:", part_2())
