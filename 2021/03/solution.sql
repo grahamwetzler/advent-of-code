2f9e34108f822fc234bd8c50db6f8edbbe33d340
Graham Wetzler <graham@wetzler.dev>
2021-12-04 07:53:38 -0600

    Add working part 1

--- 2021/03/solution.sql
+++ 2021/03/solution.sql
@@ -1,3 +1,12 @@
+create or replace function binary_to_number(a string)
+  returns float
+  language javascript
+  as
+  $$
+    return parseInt(A, 2)
+  $$
+;
+
 create table aoc.input_2021.day_03 (
     b1 integer
   , b2 integer
@@ -13,3 +22,153 @@ create table aoc.input_2021.day_03 (
   , b12 integer
 );
 
+create table aoc.input_2021.day_03_sample (
+    b1 integer
+  , b2 integer
+  , b3 integer
+  , b4 integer
+  , b5 integer
+);
+
+
+with input as (
+  select *
+    from aoc.input_2021.day_03
+)
+
+, gamma as (
+  select   binary_to_number(
+                concat(
+                  (select b1
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                , (select b2
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                , (select b3
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                , (select b4
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                , (select b5
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                , (select b6
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                , (select b7
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                , (select b8
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                , (select b9
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                , (select b10
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                , (select b11
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                , (select b12
+                     from input
+                    group by 1
+                    order by count(*) desc
+                    limit 1)
+                    )) as gamma
+)
+
+, epsilon as (
+  select   binary_to_number(
+                concat(
+                  (select b1
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                , (select b2
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                , (select b3
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                , (select b4
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                , (select b5
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                , (select b6
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                , (select b7
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                , (select b8
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                , (select b9
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                , (select b10
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                , (select b11
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                , (select b12
+                     from input
+                    group by 1
+                    order by count(*) asc
+                    limit 1)
+                    )) as epsilon
+)
+
+select (select gamma
+          from gamma) *
+       (select epsilon
+          from epsilon) as power_consumption
