ae0c96496d36bb5110ed9e29a744cd00238ac5d6
Graham Wetzler <graham@wetzler.dev>
2021-12-03 19:37:17 -0600

    Add input and table creation statement

---
+++ 2021/03/solution.sql
@@ -0,0 +1,15 @@
+create table aoc.input_2021.day_03 (
+    b1 integer
+  , b2 integer
+  , b3 integer
+  , b4 integer
+  , b5 integer
+  , b6 integer
+  , b7 integer
+  , b8 integer
+  , b9 integer
+  , b10 integer
+  , b11 integer
+  , b12 integer
+);
+
