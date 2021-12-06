ce218b5e65ab76401e268b1f23dc40110be6f43c
Graham Wetzler <graham@wetzler.dev>
2021-12-05 19:34:43 -0600

    Add part 1 solution

--- 2021/03/solution.py
+++ 2021/03/solution.py
@@ -4,30 +4,38 @@ from pathlib import Path
 
 def parse_input():
     puzzle_input = Path("2021/03/input.txt").read_text()
-    puzzle_input = """00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010"""
-    return puzzle_input
+    rows = puzzle_input.split()
+    return rows
+
+
+def bit_list_to_int(bits: list):
+    return int("".join(bits), 2)
+
+
+def count_bits(rows: list) -> list[Counter]:
+    bits = len(rows[0])
+    counts = []
+    for idx in range(bits):
+        columns = [i[idx] for i in rows]
+        counter = Counter(columns)
+        counts.append(counter)
+
+    return counts
+
+
+def part_1() -> int:
+    rows = parse_input()
+    counts = count_bits(rows)
+
+    gamma = []
+    epsilon = []
+    for count in counts:
+        (high, _), (low, _) = count.most_common()
+        gamma.append(high)
+        epsilon.append(low)
+
+    return bit_list_to_int(gamma) * bit_list_to_int(epsilon)
 
 
 if __name__ == "__main__":
-    puzzle_input = parse_input()
-    rows = puzzle_input.split()
-    bits = len(rows[0])
-
-    gamma = ""
-    epsilon = ""
-    for idx in range(bits):
-        columns = [i[idx] for i in rows]
-        gamma, epsilon += Counter(columns).keys()
-        print(gamma)
-        print(epsilon)
+    print("Part 1:", part_1())
