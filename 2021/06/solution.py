3775cefda5288b98f34b7cf01ad6f54a2644e9b1
Graham Wetzler <graham@wetzler.dev>
2021-12-06 12:22:41 -0600

    Add day 6 part 1 solution

---
+++ 2021/06/solution.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from dataclasses import dataclass
+
+
+def parse_input() -> list:
+    puzzle_input = Path("2021/06/input.txt").read_text()
+    split = puzzle_input.split(",")
+    return [int(n) for n in split]
+
+
+@dataclass
+class Fish:
+    timer: int = 8
+    days_alive: int = 0
+
+    def __repr__(self):
+        return str(self.timer)
+
+    def day(self):
+        self.days_alive += 1
+        self.timer -= 1
+        if self.timer < 0:
+            self.timer = 6
+            return Fish()
+
+
+def count_fish(days) -> int:
+    puzzle_input = parse_input()
+    fishes = [Fish(t) for t in puzzle_input]
+    for day in range(days):
+        new_fishes = []
+        for fish in fishes:
+            if new_fish := fish.day():
+                new_fishes.append(new_fish)
+        fishes += new_fishes
+        print(f"Day {day}:", fishes)
+
+    return len(fishes)
+
+
+if __name__ == "__main__":
+    print("Part 1:", count_fish(18))
