410af42ab36cc778d4fae9ac7f76a219ec852d6e
Graham Wetzler <graham@wetzler.dev>
2021-12-07 16:46:12 -0600

    Refactor into class for part 2

--- 2021/03/solution.py
+++ 2021/03/solution.py
@@ -2,40 +2,69 @@ from collections import Counter
 from pathlib import Path
 
 
-def parse_input():
-    puzzle_input = Path("2021/03/input.txt").read_text()
-    rows = puzzle_input.split()
-    return rows
+class Submarine:
+    def __init__(self):
+        self.binary_input = Path("2021/03/input.txt").read_text().split()
 
+    def bit_list_to_int(self, bits: list):
+        return int("".join(bits), 2)
 
-def bit_list_to_int(bits: list):
-    return int("".join(bits), 2)
+    def count_bits(self, rows: list) -> list[Counter]:
+        bits = len(rows[0])
+        counts = []
+        for idx in range(bits):
+            columns = [i[idx] for i in rows]
+            counter = Counter(columns)
+            counts.append(counter)
 
+        return counts
 
-def count_bits(rows: list) -> list[Counter]:
-    bits = len(rows[0])
-    counts = []
-    for idx in range(bits):
-        columns = [i[idx] for i in rows]
-        counter = Counter(columns)
-        counts.append(counter)
+    @property
+    def power_consuption(self) -> int:
+        counts = self.count_bits(self.binary_input)
 
-    return counts
+        gamma = []
+        epsilon = []
+        for count in counts:
+            (most_common, _), (least_common, _) = count.most_common()
+            gamma.append(most_common)
+            epsilon.append(least_common)
 
+        return self.bit_list_to_int(gamma) * self.bit_list_to_int(epsilon)
 
-def part_1() -> int:
-    rows = parse_input()
-    counts = count_bits(rows)
+    def calculate_life_support(self, type: str) -> int:
+        readings = self.binary_input
 
-    gamma = []
-    epsilon = []
-    for count in counts:
-        (high, _), (low, _) = count.most_common()
-        gamma.append(high)
-        epsilon.append(low)
+        idx = 0
+        while len(readings) > 1:
+            counts = self.count_bits(readings)
+            count = counts[idx]
+            (most_common, high_count), (least_common, low_count) = count.most_common()
+            if type == "oxygen":
+                if high_count == low_count:
+                    most_common = "1"
+                readings = list(filter(lambda x: x[idx] == most_common, readings))
+            elif type == "co2":
+                if high_count == low_count:
+                    least_common = "0"
+                readings = list(filter(lambda x: x[idx] == least_common, readings))
+            idx += 1
 
-    return bit_list_to_int(gamma) * bit_list_to_int(epsilon)
+        return self.bit_list_to_int(readings)
+
+    @property
+    def oxygen_generator_rating(self) -> int:
+        return self.calculate_life_support(type="oxygen")
+
+    @property
+    def co2_scrubber_rating(self) -> int:
+        return self.calculate_life_support(type="co2")
+
+    @property
+    def life_support_rating(self):
+        return self.oxygen_generator_rating * self.co2_scrubber_rating
 
 
 if __name__ == "__main__":
-    print("Part 1:", part_1())
+    print("Part 1:", Submarine().power_consuption)
+    print("Part 2:", Submarine().life_support_rating)
