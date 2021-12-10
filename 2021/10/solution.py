3ad61a94ea0a2448bb93ddc38bc64d3f43ad66c7
Graham Wetzler <graham@wetzler.dev>
2021-12-10 12:00:34 -0600

    Add solution for part 2

--- 2021/10/solution.py
+++ 2021/10/solution.py
@@ -1,18 +1,18 @@
-import re
 from pathlib import Path
+from statistics import median
 
-BRACKETS = {")": "(", "]": "[", "}": "{", ">": "<"}
+BRACKETS_OPEN_CLOSE = {"(": ")", "[": "]", "{": "}", "<": ">"}
+BRACKETS_CLOSE_OPEN = {v: k for k, v in BRACKETS_OPEN_CLOSE.items()}
 
 
 class Line:
     def __init__(self, line):
         self.line = line
+        self.first_illegal_bracket = None
+        self.missing_brackets = None
+        self._parse()
 
-    def parse(self):
-        return re.match("\[.*\]", self.line)
-
-    @property
-    def first_illegal_bracket(self):
+    def _parse(self):
         opening_brackets = []
 
         for bracket in self.line:
@@ -20,10 +20,14 @@ class Line:
                 case "(" | "[" | "{" | "<":
                     opening_brackets.append(bracket)
                 case _ as b:
-                    if opening_brackets[-1] == BRACKETS[b]:
+                    if opening_brackets[-1] == BRACKETS_CLOSE_OPEN[b]:
                         opening_brackets.pop()
                     else:
-                        return b
+                        self.first_illegal_bracket = b
+                        return self
+
+        self.missing_brackets = [BRACKETS_OPEN_CLOSE[b] for b in opening_brackets[::-1]]
+        return self
 
 
 def parse_input():
@@ -40,5 +44,21 @@ def part_1():
     return sum([scores[i] for i in illegal_characters])
 
 
+def part_2():
+    missing_bracket_scores = []
+    scores = {")": 1, "]": 2, "}": 3, ">": 4}
+    for line in parse_input():
+        if missing := Line(line).missing_brackets:
+            score = 0
+            for m in missing:
+                score *= 5
+                score += scores[m]
+
+            missing_bracket_scores.append(score)
+    
+    return median(missing_bracket_scores)
+
+
 if __name__ == "__main__":
     print("Part 1:", part_1())
+    print("Part 2:", part_2())
