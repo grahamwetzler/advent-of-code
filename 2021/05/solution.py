875bd7b241f5f27ce43c2095e22e1481a8059cd5
Graham Wetzler <graham@wetzler.dev>
2021-12-10 15:50:06 -0600

    Refactor for part 2 solution

--- 2021/05/solution.py
+++ 2021/05/solution.py
@@ -15,20 +15,31 @@ def dangerous_areas(array: np.array, threshold: int = 2) -> int:
     return (array >= threshold).sum()
 
 
-def part_1() -> int:
+def part_1_and_2():
     coordinates = parse_input()
     grid_dims = max([coodinate[0] for i in coordinates for coodinate in i]) + 1
     grid = np.zeros((grid_dims, grid_dims), dtype=int)
     for (x1, y1), (x2, y2) in coordinates:
-        x = sorted([x1, x2])
-        x[1] += 1
-        y = sorted([y1, y2])
-        y[1] += 1
-        if x1 == x2 or y1 == y2:
-            grid[slice(*y), slice(*x)] += 1
+        if (x1 == x2) or (y1 == y2):
+            if (x1 < x2) or (y1 < y2):
+                grid[x1 : x2 + 1, y1 : y2 + 1] += 1
+            elif (x1 > x2) or (y1 > y2):
+                grid[x2 : x1 + 1, y2 : y1 + 1] += 1
 
-    return dangerous_areas(grid)
+    print("Part 1:", dangerous_areas(grid))
+
+    for (x1, y1), (x2, y2) in coordinates:
+        if (x1 != x2) and (y1 != y2):
+            x_step = -1 if x1 > x2 else 1
+            y_step = -1 if y1 > y2 else 1
+
+            for x, y in zip(
+                range(x1, x2 + x_step, x_step), range(y1, y2 + y_step, y_step)
+            ):
+                grid[x, y] += 1
+
+    print("Part 2:", dangerous_areas(grid))
 
 
 if __name__ == "__main__":
-    print("Part 1:", part_1())
+    part_1_and_2()
