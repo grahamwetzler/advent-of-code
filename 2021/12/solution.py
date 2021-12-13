bc33cbda8974ca9636f81f2da2ebc07e23890ec8
Graham Wetzler <graham@wetzler.dev>
2021-12-13 14:46:50 -0600

    Refactor and add part 2 solution

--- 2021/12/solution.py
+++ 2021/12/solution.py
@@ -3,20 +3,31 @@ from pathlib import Path
 
 
 class Caves:
-    def __init__(self, puzzle_input) -> None:
+    def __init__(self, puzzle_input: str, small_cave_twice: bool) -> None:
         self.paths = self._parse_input(puzzle_input)
         self._build_graph()
-        self.paths_visted = self.dfs(cave="start", visited={"start"})
+        self.paths_visted = self.dfs(
+            cave="start",
+            visited={"start"},
+            small_cave_twice=small_cave_twice,
+        )
 
-    def dfs(self, cave, visited, counter=0) -> None:
+    def dfs(
+        self, cave: str, visited: set, small_cave_twice: bool, counter: int = 0
+    ) -> None:
         if cave == "end":
             return 1
 
         for neighbor_cave in self.graph[cave]:
             if neighbor_cave.isupper():
-                counter += self.dfs(neighbor_cave, visited)
-            elif neighbor_cave not in visited:
-                counter += self.dfs(neighbor_cave, visited | {neighbor_cave})
+                counter += self.dfs(neighbor_cave, visited, small_cave_twice)
+            else:
+                if neighbor_cave not in visited:
+                    counter += self.dfs(
+                        neighbor_cave, visited | {neighbor_cave}, small_cave_twice
+                    )
+                elif small_cave_twice and neighbor_cave not in {"start", "end"}:
+                    counter += self.dfs(neighbor_cave, visited, small_cave_twice=False)
 
         return counter
 
@@ -26,15 +37,21 @@ class Caves:
             self.graph[start].append(end)
             self.graph[end].append(start)
 
-    def _parse_input(self, puzzle_input) -> list[list[str]]:
+    def _parse_input(self, puzzle_input: str) -> list[list[str]]:
         return [row.split("-") for row in puzzle_input.split("\n") if row]
 
 
-def part_1(puzzle_input):
-    caves = Caves(puzzle_input)
+def part_1(puzzle_input: str) -> int:
+    caves = Caves(puzzle_input, small_cave_twice=False)
+    return caves.paths_visted
+
+
+def part_2(puzzle_input: str) -> int:
+    caves = Caves(puzzle_input, small_cave_twice=True)
     return caves.paths_visted
 
 
 if __name__ == "__main__":
     puzzle_input = Path("2021/12/input.txt").read_text()
     print("Part 1:", part_1(puzzle_input))
+    print("Part 2:", part_2(puzzle_input))
--- 2021/12/test_solution.py
+++ 2021/12/test_solution.py
@@ -1,4 +1,4 @@
-from solution import part_1
+from solution import part_1, part_2
 
 test_input_1 = """
 start-A
@@ -20,10 +20,35 @@ LN-dc
 HN-end
 kj-sa
 kj-HN
-kj-dc
+kj-dc"""
+
+test_input_3 = """
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
 """
 
 
 def test_part_1():
     assert part_1(test_input_1) == 10
     assert part_1(test_input_2) == 19
+
+
+def test_part_2():
+    assert part_2(test_input_2) == 103
+    assert part_2(test_input_3) == 3509
