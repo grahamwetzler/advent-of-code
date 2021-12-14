23f51e8baa9b21c526d5233ed02ad9f4b7387cfa
Graham Wetzler <graham@wetzler.dev>
2021-12-14 09:11:15 -0600

    Add template

---
+++ 2021/dd/solution.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+def part_1():
+    ...
+
+
+def part_2():
+    ...
+
+
+if __name__ == "__main__":
+    ...
---
+++ 2021/dd/test_solution.py
@@ -0,0 +1,12 @@
+from solution import part_1, part_2
+
+test_input_1 = """
+"""
+
+
+def test_part_1():
+    assert part_1()
+
+
+def test_part_2():
+    assert part_2()
