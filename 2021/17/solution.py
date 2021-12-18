4700651417890aa9eb408defc4a596a3f2c774ea
Graham Wetzler <graham@wetzler.dev>
2021-12-17 20:53:10 -0600

    Update for part 2

--- 2021/17/solution.py
+++ 2021/17/solution.py
@@ -17,18 +17,15 @@ def is_in_target_area(x: int, y: int) -> bool:
     return False
 
 
-def launch_probe(x_velocity: int, y_velocity: int) -> Optional[int]:
-    x, y = (0, 0)
+def launch_probe(x_velocity: int, y_velocity: int) -> tuple[bool, Optional[int]]:
+    x, y = 0, 0
     y_values = []
     while True:
         x += x_velocity
-        if is_in_target_area(x, y):
-            return max(y_values)
-
         y += y_velocity
         y_values.append(y)
         if is_in_target_area(x, y):
-            return max(y_values)
+            return True, max(y_values)
 
         if x_velocity > 0:
             x_velocity -= 1
@@ -38,23 +35,31 @@ def launch_probe(x_velocity: int, y_velocity: int) -> Optional[int]:
         y_velocity -= 1
 
         if x > x_max or y < y_min:
-            return None
+            return False, None
 
 
 def part_1() -> int:
     heighest_y = []
     for x_velocity, y_velocity in itertools.product(range(0, 250), range(-250, 250)):
-        if y_values := launch_probe(x_velocity, y_velocity):
+        _, y_values = launch_probe(x_velocity, y_velocity)
+        if y_values:
             heighest_y.append(y_values)
 
     return max(heighest_y)
 
 
-def part_2():
-    ...
+def part_2() -> int:
+    within_target = set()
+    for x_velocity, y_velocity in itertools.product(range(0, 250), range(-250, 250)):
+        hit, _ = launch_probe(x_velocity, y_velocity)
+        if hit:
+            within_target.add((x_velocity, y_velocity))
+
+    return len(within_target)
 
 
 if __name__ == "__main__":
     puzzle_input = Path("2021/17/input.txt").read_text()
     x_min, x_max, y_min, y_max = parse_input(puzzle_input)
     print("Part 1:", part_1())
+    print("Part 2:", part_2())
