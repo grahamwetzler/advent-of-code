fe714a83951d074b66515c94ab0b46fd044b5827
Graham Wetzler <graham@wetzler.dev>
2021-12-19 20:33:29 -0600

    Update for part 2 solution

--- 2021/16/solution.py
+++ 2021/16/solution.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from math import prod
 from pathlib import Path
 
 
@@ -38,6 +39,7 @@ def decode_packets_by_length(packet: str, index: int) -> tuple[list, int]:
         sub_packets.append(sub_packet)
         bits += new_index - index
         index = new_index
+
     return sub_packets, index
 
 
@@ -48,6 +50,7 @@ def decode_packets_by_number(packet: str, index: int) -> tuple[list, int]:
     for _ in range(number_sub_packets):
         sub_packet, index = parse(packet, index)
         sub_packets.append(sub_packet)
+
     return sub_packets, index
 
 
@@ -67,28 +70,57 @@ def parse(packet: Packet, index: int = 0) -> tuple[Packet, int]:
     else:
         sub_packets, index = decode_packets_by_number(packet, index)
 
-    value = decode_literal_value(packet, index)
+    value = calculate_packet_value(type_id, sub_packets)
+
     return Packet(version, type_id, value, sub_packets), index
 
 
-def sum_versions(packet):
+def sum_versions(packet: Packet) -> int:
     total = packet.version
     for sub_packet in packet.sub_packets:
         total += sum_versions(sub_packet)
+
     return total
 
 
+def calculate_packet_value(type_id: int, sub_packets: list) -> int:
+    values = [packet.value for packet in sub_packets]
+    if type_id in [0, 1, 2, 3]:
+        if type_id == 0:
+            value = sum(values)
+        elif type_id == 1:
+            value = prod(values)
+        elif type_id == 2:
+            value = min(values)
+        elif type_id == 3:
+            value = max(values)
+    else:
+        first, second = values
+        if type_id == 5:
+            value = 1 if first > second else 0
+        elif type_id == 6:
+            value = 1 if first < second else 0
+        else:
+            value = 1 if first == second else 0
+
+    return value
+
+
 def part_1(packet: str) -> int:
-    packet_binary = hex_to_bin(packet)
-    parsed_packets, _ = parse(packet_binary)
+    packet_as_binary = hex_to_bin(packet)
+    outer_packet, _ = parse(packet_as_binary)
 
-    return sum_versions(parsed_packets)
+    return sum_versions(outer_packet)
 
 
-def part_2(puzzle_input: str) -> int:
-    ...
+def part_2(packet: str) -> int:
+    packet_as_binary = hex_to_bin(packet)
+    outer_packet, _ = parse(packet_as_binary)
+
+    return outer_packet.value
 
 
 if __name__ == "__main__":
     puzzle_input = Path("2021/16/input.txt").read_text().strip()
     print("Part 1:", part_1(puzzle_input))
+    print("Part 2:", part_2(puzzle_input))
