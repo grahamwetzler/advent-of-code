63e986b7fe2268a01ebbbab35ce49c29a56bedee
Graham Wetzler <graham@wetzler.dev>
2021-12-22 09:03:35 -0600

    Add part 2 solution

--- 2021/21/solution.py
+++ 2021/21/solution.py
@@ -1,66 +1,63 @@
+from dataclasses import dataclass
+from functools import lru_cache
+from itertools import cycle, product
 from pathlib import Path
 
 
-class DeterministicDie:
-    def __init__(self) -> None:
-        self.state = 0
-        self.times_rolled = 0
+@dataclass
+class Player:
+    score: int = 0
+    wins: int = 0
 
-    def roll3(self):
-        values = []
-        for _ in range(3):
-            values.append(self._roll())
+    def __init__(self, id: int, position: int) -> None:
+        self.id = id
+        self.position = position
+        self.starting_postition = position
 
-        return sum(values)
-
-    def _roll(self):
-        self.state += 1
-        self.times_rolled += 1
-
-        if self.state > 100:
-            self.state = 1
-
-        return self.state
+    def reset(self):
+        self.position = self.starting_postition
+        self.score = 0
 
 
-def move(start, roll):
-    _, remainder = divmod(start + roll, 10)
-
-    if remainder == 0:
-        return 10
-
-    return remainder
+def move(position: int, score: int, roll: int) -> tuple[int]:
+    position = (position + roll - 1) % 10 + 1
+    return position, score + position
 
 
 def part_1(starting_positions):
-    die = DeterministicDie()
-    p1, p2 = starting_positions
-
-    p1 = move(p1, die.roll3())
-    p2 = move(p2, die.roll3())
-
-    scores = [p1, p2]
+    die = cycle(range(1, 101))
+    rolls = 0
+    players = [Player(player, position) for player, position in starting_positions]
 
     while True:
-
-        p1 = move(p1, die.roll3())
-        scores[0] += p1
-        if scores[0] >= 1000:
-            break
-
-        p2 = move(p2, die.roll3())
-        scores[1] += p2
-        if scores[1] >= 1000:
-            break
-
-    return min(scores) * die.times_rolled
+        for player in players:
+            roll = next(die) + next(die) + next(die)
+            rolls += 3
+            player.position, player.score = move(player.position, player.score, roll)
+            if player.score >= 1000:
+                return min(player.score for player in players) * rolls
 
 
-def part_2():
-    ...
+def part_2(p1, p2):
+    @lru_cache(maxsize=None)
+    def count_wins(p1, p2):
+        p1_wins = p2_wins = 0
+        for a, b, c in product(range(1, 4), repeat=3):
+            _, score = roll_p1 = move(*p1, a + b + c)
+            if score >= 21:
+                p1_wins += 1
+            else:
+                roll_p2_wins, roll_p1_wins = count_wins(p2, roll_p1)
+                p1_wins += roll_p1_wins
+                p2_wins += roll_p2_wins
+
+        return p1_wins, p2_wins
+
+    return max(count_wins((p1, 0), (p2, 0)))
 
 
 if __name__ == "__main__":
     puzzle_input = Path("2021/21/input.txt").read_text().splitlines()
-    starting_positions = [int(p[-1]) for p in puzzle_input]
+    starting_positions = [(idx, int(p[-1])) for idx, p in enumerate(puzzle_input, 1)]
     print("Part 1:", part_1(starting_positions))
+    print("Part 2:", part_2(7, 5))
